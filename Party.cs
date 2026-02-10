8993bfac01c2aedacc40c3cc308510726401d681
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-10 12:08:31 -0600

    Refactor Guests to use array initialization
    Changed Guests from a property to an array initialization.

--- Party.cs
+++ Party.cs
@@ -11,12 +11,13 @@ public class Party
             this.lastName = lastName;
         }
     }
-    public string[] Guests{
-        Person person1 = new Person("Abdul", "Ahmed"),
-        Person person2 = new Person("Name", "Last"),
-        Person person3 = new Person("Ramsey", "Freeman")
+    Person[] Guests = new Person[]{
+        new Person("Abdul", "Ahmed"),
+        new Person("Name", "Last"),
+        new Person("Ramsey", "Freeman")
     }
 }
+
 
 
 
