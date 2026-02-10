966b114099de0bde6506567d588c2068824ac047
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-10 11:49:23 -0600

    Define Person class and add guest initialization
    Added Person class and initialized guest list.

--- Party.cs
+++ Party.cs
@@ -2,4 +2,21 @@
 public class Party
 {
     // Add your code here
+    public class Person{
+        public string firstName;
+        public string lastName;
+
+        public Person(string firstName, string lastName){
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+    }
+    public string[] Guests{
+        Person person1 = new Person("Abdul", "Ahmed");
+        Person person2 = new Person("Name", "Last");
+        Person person3 = new Person("Ramsey", "Freeman");
+        
+        return Guests;
+    }
 }
+
