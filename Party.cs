e5a36361efa173926537230c1031af08f430c689
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-12 13:54:47 -0600

    Correct Person constructor and Guests initialization
    Fixed constructor parameter names and added missing commas in Guests array.

--- Party.cs
+++ Party.cs
@@ -6,17 +6,18 @@ public class Party
         public string firstName;
         public string lastName;
 
-        public Person(public string firstName { get; }, public string lastName { get; }){
-            this.firstName = firstName;
-            this.lastName = lastName;
+        public Person(public string FirstName { get; }, public string LastName { get; }){
+            this.FirstName = firstName;
+            this.LastName = lastName;
         }
     }
     public Person[] Guests { get; } = new Person[]{
-        new Person("Abdul", "Ahmed")
-        new Person("Name", "Last")
-        new Person("Ramsey", "Freeman")
+        new Person("Abdul", "Ahmed"),
+        new Person("Name", "Last"),
+        new Person("Ramsey", "Freeman"),
     }
 }
+
 
 
 
