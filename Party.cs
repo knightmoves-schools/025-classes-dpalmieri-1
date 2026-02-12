30af0309f335e70c07b762878348c18f5b6d4ccd
D'Angelo <dpalmieri@anamosa.k12.ia.us>
2026-02-12 14:06:38 -0600

    Refactor Party and Person class structure
    Refactor Person class to use properties and fix constructor parameters.

--- Party.cs
+++ Party.cs
@@ -2,14 +2,14 @@
 public class Party
 {
     // Add your code here
-    
-    public string firstName;
-    public string lastName;
-
     public class Person{
-        public Person(public string FirstName { get; }, public string LastName { get; }){
-            this.FirstName = firstName;
-            this.LastName = lastName;
+        
+        public string FirstName { get; };
+        public string LastName { get; };
+
+        public Person(firstName, lastName){
+            this.firstName = FirstName;
+            this.lastName = LastName;
         }
     }
     public Person[] Guests { get; } = new Person[]{
